b0d7d234e2d7d61fd31b0ad57b2fc682a37fef5f
travnettech <61828449+travnettech@users.noreply.github.com>
2022-05-06 12:20:43 +0100

    me remove

--- hesa_dc/www/me.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from frappe import _
-
-no_cache = 1
-
-def get_context(context):
-    if frappe.session.user=='Guest':
-        frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
-    context.current_user = frappe.get_doc("User", frappe.session.user)

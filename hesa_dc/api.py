8a6ef5da05b2f079873f854d838bc3f54323e3b9
travnettech <61828449+travnettech@users.noreply.github.com>
2022-05-24 09:45:25 +0100

    Update api.py

--- hesa_dc/api.py
+++ hesa_dc/api.py
@@ -77,7 +77,7 @@ def create_hesa_dc_sa_return_file(returnType, submissionPurpose, academicYear=No
     # else:
     #     param = (academicYear)
     all_program_data = frappe.db.sql(program_sql,as_dict=1)
-    return program_sql
+    return all_program_data
     # all_student_data = frappe.db.sql('''SELECT * FROM tabStudent''',as_dict=1)
 
     root = minidom.Document()
